4d87fe08404d698fa0c1944ffc1056e6e08d207a
Vaibhav Jain <vaibhav2.jain@paytmmall.com>
2017-12-22 16:07:43 +0530

    hehe

--- vars/pipeline.groovy
+++ vars/pipeline.groovy
@@ -1,40 +1,6 @@
 #!/usr/bin/env groovy
 
-def call(body) {
+def call(String name = 'human') {
+  echo "Hello, ${name}."
+}
 
-        def config = [:]
-        body.resolveStrategy = Closure.DELEGATE_FIRST
-        body.delegate = config
-        body()
-
-        node {
-            // Clean workspace before doing anything
-            deleteDir()
-
-            try {
-                stage ('Clone') {
-                    sh "echo 'checkout ${config.projectName} ...'"
-                }
-                stage ('Build') {
-                    sh "echo 'building ${config.projectName} ...'"
-                }
-                stage ('Tests') {
-                    parallel 'static': {
-                        sh "echo 'shell scripts to run static tests...'"
-                    },
-                    'unit': {
-                        sh "echo 'shell scripts to run unit tests...'"
-                    },
-                    'integration': {
-                        sh "echo 'shell scripts to run integration tests...'"
-                    }
-                }
-                stage ('Deploy') {
-                    sh "echo 'deploying to server ${config.serverDomain}...'"
-                }
-            } catch (err) {
-                currentBuild.result = 'FAILED'
-                throw err
-            }
-        }
-    }
