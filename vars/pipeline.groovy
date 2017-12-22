18ab9200f89d2b18dc3e7138c81c2ab708c49ba2
Vaibhav Jain <vaibhav2.jain@paytmmall.com>
2017-12-22 13:36:29 +0530

    test commit Test

--- vars/pipeline.groovy
+++ vars/pipeline.groovy
@@ -1,6 +1,25 @@
 #!/usr/bin/env groovy
 
-def call(String name = 'human') {
-  echo "Hello, ${name}."
-}
+def call(body) {
+
+        def config = [:]
+        body.resolveStrategy = Closure.DELEGATE_FIRST
+        body.delegate = config
+        body()
+
+        node {
+            // Clean workspace before doing anything
+            deleteDir()
+
+            try {
+                stage ('Clone') {
+                echo 'Hello world=========================aaaaaaaaaaaa============='    
+                }
+                }
+            } catch (err) {
+                currentBuild.result = 'FAILED'
+                throw err
+            }
+        }
+   
 
