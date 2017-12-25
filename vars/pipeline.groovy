4df334f51e17dc1c15de6ec2de11c10e5cdf49fa
Vaibhav Jain <vaibhavjain882@gmail.com>
2017-12-25 15:24:26 +0530

    Update pipeline.groovy

--- vars/pipeline.groovy
+++ vars/pipeline.groovy
@@ -1,13 +1,7 @@
 #!/usr/bin/env groovy
 
 def call(String name = 'human') {
-    agent any
-    stages {
-        stage('Build') { 
-            steps { 
-                sh 'echo "Hello, ${name}."' 
-            }
-        }
-    }  
+  echo "Hello, ${name}."
 }
+
 
